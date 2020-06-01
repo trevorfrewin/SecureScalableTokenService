2bdf98450bbcb1bf528af6e7d184b3dec41127f3
Trevor Frewin <tjfrewin@gmail.com>
2020-06-01 17:09:10 +0100

    Implement a simple ConfigurationManagement Source

--- SSTS.Api.Query/Startup.cs
+++ SSTS.Api.Query/Startup.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using SSTS.Library.ConfigurationManagement;
 
 namespace SSTS.Api.Query
 {
@@ -26,6 +20,7 @@ namespace SSTS.Api.Query
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IConfigurationManagementSource, ConfigurationManagementSource>();
             services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
