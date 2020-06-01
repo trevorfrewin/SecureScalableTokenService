8ae0c3e52b6dde342f62d899210a1ef195124148
Trevor Frewin <tjfrewin@gmail.com>
2020-06-01 13:01:44 +0100

    Changed namespace scope order.

--- SSTS.Api.Query/Startup.cs
+++ SSTS.Api.Query/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace SSTS.QueryApi
+namespace SSTS.Api.Query
 {
     public class Startup
     {
